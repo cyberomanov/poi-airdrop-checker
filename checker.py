7f3a6f7ab3408d5c01b0b736fe8db6e5efd0b070
cyberomanov <romanoff_1@bk.ru>
2024-03-24 14:43:51 +0200

    v1.0 - save eligible

--- checker.py
+++ checker.py
@@ -3,7 +3,7 @@ from loguru import logger
 
 from datatypes.response import PoiResponse
 from tools.add_logger import add_logger
-from tools.other import read_file
+from tools.other import read_file, remove_file_if_exists
 
 
 def get_claimable_balance(session: requests.Session, address: str):
@@ -20,16 +20,19 @@ if __name__ == '__main__':
     add_logger()
     try:
         addresses = read_file(path='data/address.txt')
+        remove_file_if_exists(path='log/eligible.txt')
 
         poi_total = 0
-        with requests.session() as session:
-            for address in addresses:
-                claimable_balance = get_claimable_balance(session=session, address=address)
-                if claimable_balance:
-                    poi_total += claimable_balance
-                    logger.success(f"{address}: {claimable_balance} $POI.")
-                else:
-                    logger.info(f"{address}: 0 $POI.")
+        with open('log/eligible.txt', 'a') as file:
+            with requests.session() as session:
+                for address in addresses:
+                    claimable_balance = get_claimable_balance(session=session, address=address)
+                    if claimable_balance:
+                        poi_total += claimable_balance
+                        logger.success(f"{address}: {claimable_balance} $POI.")
+                        file.write(address + '\n')
+                    else:
+                        logger.info(f"{address}: 0 $POI.")
 
         logger.info(f"total: {poi_total} $POI.")
     except Exception as e:
--- tools/other.py
+++ tools/other.py
@@ -1,4 +1,12 @@
+import os
+
+
 def read_file(path: str):
     with open(path) as file:
         not_empty = [line for line in file.read().splitlines() if line]
     return list(set(not_empty))
+
+
+def remove_file_if_exists(path: str):
+    if os.path.exists(path):
+        os.remove(path)
